a19beb30778428d791f00f65b8ab8f231c0dd5b2
25n71w <39124351+25n71w@users.noreply.github.com>
2021-10-12 17:14:29 -0400

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using Lab3.models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Razor() => View();
+
+        [HttpPost]
+        public IActionResult Count()
+        {
+            
+            ViewData["numOfBottles"] = Request.Form["numOfBottles"];
+            return View(); 
+        }
+
+        public IActionResult CreatePerson() => View();
+
+        [HttpPost]
+        public IActionResult DisplayPerson(Person person) 
+        {
+            
+            
+            return View(person);
+        }
+        public IActionResult Error()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Models/Person.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab3.models
+{
+    public class Person
+    {
+        public string FirstName
+        {
+            get;
+            set;
+        }
+        public string LastName
+        {
+            get;
+            set;
+        }
+        public int Age
+        {
+            get;
+            set;
+        }
+        public string EmailAddress
+        {
+            get;
+            set;
+        }
+        //DOB
+        public string Password
+        {
+            get;
+            set;
+        }
+        public string PersDescription
+        {
+            get;
+            set;
+        }
+    }
+}
